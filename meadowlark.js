57a680be07f05b1fc427e78680612435fa52f46e
Alexander <sashaalenushka@gmail.com>
2024-04-04 02:42:06 +0400

    Произведен рефакторинг кода для маршрутов, написаны модульные тесты для обработчиков маршрутов и интеграционный тест

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { engine: expressHandlebars } = require('express-handlebars')
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -14,21 +14,18 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(`Express запущен на http://localhost:${port}; нажмите ctrl+c для завершения.`))
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log(`Express запущен на http://localhost:${port}; нажмите ctrl+c для завершения.`)
+  })
+} else {
+  module.exports = app
+}
